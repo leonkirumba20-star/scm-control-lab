872a3357471fe2f4856477318e7825e67f17cc65
Leon <Leonkirumba20@gmail.com>
2025-12-26 22:17:09 +0300

    Add calc.py - Basic Calculator v.1.0.0

---
+++ calc.py
@@ -0,0 +1,13 @@
+# calc.py - Basic Calculator v1.0.0
+
+def add(a, b):
+    return a + b
+
+def subtract(a, b):
+    return a - b
+
+# Quick test when running the script directly
+if __name__ == "__main__":
+    print("Basic Calculator v1.0.0")
+    print("5 + 3 =", add(5, 3))
+    print("5 - 3 =", subtract(5, 3))
